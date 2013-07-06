87fedb53895535b0f8af913e99d3e66ff05f9b83
Jake Teton-Landis <just.1.jake@gmail.com>
2013-07-06 14:50:39 -0700

    seperate server and session

--- app/models/server.go
+++ app/models/server.go
@@ -3,54 +3,28 @@ package models
 
 // nice imap library
 // http://godoc.org/code.google.com/p/go-imap/go1/imap
-import "code.google.com/p/go-imap/go1/imap"
-import "time"
-
-/////// session //////////
-// a mapping of a session id -> IMAP server list
-type SessionID string
-type Session   map[string]*Server
-
-// in-memory datastore
-var sessions = make(map[SessionID]Session)
-
-// create
-func NewOrCreateSession(id SessionID) Session {
-    ses, ok := sessions[id]
-    if ok {
-        return ses
-    }
-
-    // create new session
-    ses = make(Session)
-    sessions[id] = ses
-    return ses
-}
-
-// remove a session from the datastore
-func DestroySession(id SessionID) {
-    if ses, ok := sessions[id]; ok {
-        // close all connections
-        for _, server := range ses {
-            server.Close()
-        }
-    }
-
-    delete(sessions, id)
-}
-
+import (
+    "code.google.com/p/go-imap/go1/imap"
+    "time"
+    "fmt"
+    "log"
+)
 
 ///////// server ///////////
 // connection to an IMAP server
 // totally in-memory
 
-const ServerLogoutTimeout = 10 * time.Second
+const ServerLogoutPause = 10 * time.Second
+
+// we disconnect from the IMAP server after this much time with no request to Connect()
+const NoUsageDisconnect = 20 * time.Minute
 
 type Server struct {
     Hostname string
     Username string
     Password string
     client *imap.Client
+    disconnectTimer *time.Timer
 }
 
 // esablish an IMAP connection
@@ -58,6 +32,7 @@ type Server struct {
 func (s *Server) Connect() (*imap.Client, error) {
     // re-use existing connection
     if s.client != nil {
+        s.disconnectTimer.Reset()
         return s.client, nil
     }
 
@@ -74,6 +49,8 @@ func (s *Server) Connect() (*imap.Client, error) {
             s.Close() 
             return nil, err
         }
+    } else {
+        log.Printf("Connection %v: TLS DISABLED", c)
     }
 
     // log in
@@ -85,15 +62,25 @@ func (s *Server) Connect() (*imap.Client, error) {
         }
     }
 
+    // auto-disconnect after a certain timeout
+    s.disconnectTimer = time.AfterFunc(NoUsageDisconnect, func () {
+        s.Close()
+    })
+
     return client, nil
 }
 
 func (s *Server) Close() (error) {
     if s.client != nil {
-        _, err := s.client.Logout(ServerLogoutTimeout)
+        _, err := s.client.Logout(ServerLogoutPause)
         return err
     }
     s.client = nil
     return nil
 
+}
+
+// list the mailboxes on the server
+func (s *Server) Mailboxes ([]string, error) {
+    return nil, fmt.Errorf("not yet implemented")
 }
--- app/models/session.go
+++ app/models/session.go
@@ -1,11 +1,6 @@
 // Per-user database
 package models
 
-// nice imap library
-// http://godoc.org/code.google.com/p/go-imap/go1/imap
-import "code.google.com/p/go-imap/go1/imap"
-import "time"
-
 /////// session //////////
 // a mapping of a session id -> IMAP server list
 type SessionID string
@@ -37,63 +32,4 @@ func DestroySession(id SessionID) {
     }
 
     delete(sessions, id)
-}
-
-
-///////// server ///////////
-// connection to an IMAP server
-// totally in-memory
-
-const ServerLogoutTimeout = 10 * time.Second
-
-type Server struct {
-    Hostname string
-    Username string
-    Password string
-    client *imap.Client
-}
-
-// esablish an IMAP connection
-// TODO: establish TLS with sensible tls.Config (is this required? example uses nil :\ )
-func (s *Server) Connect() (*imap.Client, error) {
-    // re-use existing connection
-    if s.client != nil {
-        return s.client, nil
-    }
-
-    // establish new connection
-    c, err := imap.Dial(s.Hostname)
-    if err != nil { return nil, err }
-
-    s.client = c
-
-    // enable encryption if supported
-    if c.Caps["STARTTLS"] {
-        _, err := c.StartTLS(nil)
-        if err != nil { 
-            s.Close() 
-            return nil, err
-        }
-    }
-
-    // log in
-    if c.State() == imap.Login {
-        _, err := c.Login(s.Username, s.Password)
-        if err != nil {
-            s.Close()
-            return nil, err
-        }
-    }
-
-    return client, nil
-}
-
-func (s *Server) Close() (error) {
-    if s.client != nil {
-        _, err := s.client.Logout(ServerLogoutTimeout)
-        return err
-    }
-    s.client = nil
-    return nil
-
 }
