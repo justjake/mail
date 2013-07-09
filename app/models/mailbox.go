a6d19886aefb66c0897b2ab10cd981523a027d1b
Jake Teton-Landis <just.1.jake@gmail.com>
2013-07-09 01:34:39 -0700

    multi-part message retrieval and parsing

--- app/models/mailbox.go
+++ app/models/mailbox.go
@@ -6,7 +6,9 @@ import (
     "container/list"
     "bytes"
     "io"
+    "io/ioutil"
     "fmt"
+    "mime/multipart"
 )
 
 // mailbox + message, ties back to server\
@@ -93,25 +95,39 @@ func (m *Mailbox) Update() (newMessages *list.List /* *Message */, err error) {
 
 
 ///////////////////////////////////////////////////////////////////////////////
-// Mesages
+// Mesage
 // represents a single email
-// TODO: provide a way to easily parse and store multi-part bodies
 type Message struct {
-    UID uint32
-    server *Server
-    mailbox *Mailbox
-    Header mail.Header
-    Body   io.Reader
+    UID       uint32
+    server    *Server
+    mailbox   *Mailbox
+    Header    mail.Header
+    BodyData  []byte    `json:"-"` // field ignored in JSON -- use Body
+                                   // instead
+    Body      []*Part      // populated with MIB body sections or just
+                           // body text
 }
+
+
+
 
 // messages are usually created with just header information
 // this method downloads the actual body of the message from the server,
 // optionally marking the message as '\Seen', in IMAP terms.
-func (m *Message) RetrieveBody(setRead bool) (body io.Reader, err error) {
+func (m *Message) RetrieveBody(setRead bool) (body []byte, err error) {
     // cache
-    if m.Body != nil {
-        return m.Body, nil
+    if m.BodyData != nil {
+        return m.BodyData, nil
     }
+
+    // what will our FETCH request?
+    var requestType string
+    if setRead {
+        requestType = "BODY[TEXT]"
+    } else {
+        requestType = "BODY.PEEK[TEXT]"
+    }
+
 
     c, err := m.server.Connect()
     if err != nil { return }
@@ -119,14 +135,66 @@ func (m *Message) RetrieveBody(setRead bool) (body io.Reader, err error) {
     // fetch message by UID
     set, err := imap.NewSeqSet(fmt.Sprintf("%d", m.UID))
     if err != nil { return }
-    cmd, err := imap.Wait(c.UIDFetch(set, "RFC822.BODY"))
+    cmd, err := imap.Wait(c.UIDFetch(set, requestType))
     if err != nil { return }
 
-    // turn body into an io.Reader then return
+    // save response data to struct
     rsp := cmd.Data[0]
-    bodyBytes := imap.AsBytes(rsp.MessageInfo().Attrs["RFC822.BODY"])
-    body = bytes.NewReader(bodyBytes)
-    m.Body = body
+    info := rsp.MessageInfo()
+    m.BodyData = imap.AsBytes(info.Attrs["BODY[TEXT]"])
+    return m.BodyData, nil
+}
 
-    return body, nil
+///////////////////////////////////////////////////////////////////////////////
+// Part
+// basically an atattchment in the email
+type Part struct {
+    MimeType string
+    Data     []byte
+}
+
+// parse the raw RFC822.BODY bytes into seperate attatchment pieces
+// if the body is not a multi-part body, this will still return a
+// lenght-one slice of parts.
+// this is how you get parts.
+func (m *Message) GetParts() (parts []*Part, err error) {
+    // can't parse the body unless we have it
+    if m.BodyData == nil {
+        return nil, fmt.Errorf("Cannot parse nil body")
+    }
+
+    content_type := m.Header.Get("Content-Type")
+    boundry := m.Header.Get("boundry")
+
+    // only parse multipart/alternative types
+    if content_type == "multipart/alternative" {
+        if boundry == "" {
+            return nil, fmt.Errorf("Boundry was an empty string")
+        }
+
+        rdr := multipart.NewReader(bytes.NewReader(m.BodyData), boundry)
+        parts := make([]*Part, 2)
+
+        // loop until we reach the end of the multi-part message
+        for part, err := rdr.NextPart(); err != io.EOF; part, err = rdr.NextPart() {
+            if err != nil {
+                return parts, fmt.Errorf("Error while parsing multipart mail: %v", err)
+            }
+
+            // read all of the part's body data into data
+            data, err := ioutil.ReadAll(part)
+            if err != nil { return parts, err }
+
+            // save everything in the part
+            my_part := &Part{part.Header.Get("Content-Type"), data }
+            parts = append(parts, my_part)
+        }
+        return parts, nil
+    }
+
+    // base case: not a multipart message
+    // still, read the body as a byte array and encode its content-type
+    if content_type == "" { content_type = "text/plain" }
+    singular_part := &Part{content_type, m.BodyData}
+    return []*Part{singular_part}, nil
 }
